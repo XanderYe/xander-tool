9a01aa45c950d2fae6575136738ad6559ce9ab69
叶振东 <XanderYe@outlook.com>
2020-03-02 15:57:43 +0800

    添加socket和hex工具类

---
+++ src/main/java/cn/xanderye/util/HexUtil.java
@@ -0,0 +1,48 @@
+package cn.xanderye.util;
+
+/**
+ * Created on 2020/3/2.
+ *
+ * @author 叶振东
+ */
+public class HexUtil {
+
+    private static final String HEX_STRING = "0123456789ABCDEF";
+
+    /**
+     * byte数组转十六进制字符串
+     *
+     * @param bytes
+     * @return java.lang.String
+     * @author yezhendong
+     * @date 2019/8/6
+     */
+    public static String byteArrayToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(bytes[i] & 0xFF);
+            if (hex.length() < 2) {
+                sb.append(0);
+            }
+            sb.append(hex.toUpperCase()).append(" ");
+        }
+        if (sb.length() > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 字符串转成字节流
+     */
+    public static byte[] hexStringToByteArray(String hex) {
+        int len = (hex.length() / 2);
+        byte[] result = new byte[len];
+        char[] achar = hex.toCharArray();
+        for (int i = 0; i < len; i++) {
+            int pos = i * 2;
+            result[i] = (byte) (HEX_STRING.indexOf(achar[pos]) << 4 | HEX_STRING.indexOf(achar[pos + 1]));
+        }
+        return result;
+    }
+}
---
+++ src/main/java/cn/xanderye/util/SocketUtil.java
@@ -0,0 +1,46 @@
+package cn.xanderye.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created on 2020/3/2.
+ *
+ * @author 叶振东
+ */
+public class SocketUtil {
+
+    /**
+     * 读取数据方法
+     *
+     * @param inputStream
+     * @return byte[]
+     * @author yezhendong
+     * @date 2019/8/6
+     */
+    public static byte[] read(InputStream inputStream) {
+        return read(inputStream, null);
+    }
+
+    /**
+     * 读取定长数据方法
+     *
+     * @param inputStream
+     * @return byte[]
+     * @author yezhendong
+     * @date 2019/8/6
+     */
+    public static byte[] read(InputStream inputStream, Integer length) {
+        try {
+            // 避免获取0的情况
+            int firstByte = inputStream.read();
+            length = length == null ? inputStream.available() + 1 : length;
+            byte[] bytes = new byte[length];
+            bytes[0] = (byte) firstByte;
+            inputStream.read(bytes, 1, length - 1);
+            return bytes;
+        } catch (IOException e) {
+            return new byte[0];
+        }
+    }
+}
