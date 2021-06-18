925ddd37f14c32d6243d220efcf9244d3068d939
XanderYe <XanderYe@outlook.com>
2021-06-18 09:20:47 +0800

    邮件工具类增加多个收件人和抄送

--- src/main/java/cn/xanderye/util/EmailUtil.java
+++ src/main/java/cn/xanderye/util/EmailUtil.java
@@ -8,6 +8,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -35,7 +36,7 @@ public class EmailUtil {
     }
 
     /**
-     * 发送邮件
+     * 发送邮件给一个地址
      * @param title
      * @param content
      * @param toEmail
@@ -43,7 +44,7 @@ public class EmailUtil {
      * @author XanderYe
      * @date 2021/4/22
      */
-    public static void sendEmail(String title, String content, String toEmail) throws MessagingException {
+    public static void sendSimpleEmail(String title, String content, String toEmail) throws MessagingException {
         Session session = Session.getInstance(MAIL_PROPERTIES);
         session.setDebug(true);
         Message msg = new MimeMessage(session);
@@ -57,6 +58,57 @@ public class EmailUtil {
         // 收件人
         msg.setRecipient(Message.RecipientType.TO,
                 new InternetAddress(toEmail));
+        msg.saveChanges();
+
+        Transport transport = session.getTransport();
+        transport.connect(username,password);
+        transport.sendMessage(msg, msg.getAllRecipients());
+        transport.close();
+    }
+
+    /**
+     *
+     * @param title
+     * @param content
+     * @param toEmailList 收件人
+     * @param carbonCopyList 抄送
+     * @return void
+     * @author XanderYe
+     * @date 2021/6/18
+     */
+    public static void sendEmail(String title, String content, List<String> toEmailList, List<String> carbonCopyList) throws MessagingException {
+        if (toEmailList == null || toEmailList.isEmpty()) {
+            throw new RuntimeException("接收人不为空");
+        }
+        Session session = Session.getInstance(MAIL_PROPERTIES);
+        session.setDebug(true);
+        Message msg = new MimeMessage(session);
+
+        InternetAddress[] addressesTo = new InternetAddress[toEmailList.size()];
+        for (int i = 0; i < toEmailList.size(); i++) {
+            addressesTo[i] = new InternetAddress(toEmailList.get(i));
+        }
+
+        InternetAddress[] addressesCc = null;
+        if (carbonCopyList != null && !carbonCopyList.isEmpty()) {
+            addressesCc = new InternetAddress[carbonCopyList.size()];
+            for (int i = 0; i < carbonCopyList.size(); i++) {
+                addressesCc[i] = new InternetAddress(carbonCopyList.get(i));
+            }
+        }
+
+        // 标题
+        msg.setSubject(title);
+        // 内容
+        msg.setText(content);
+        // 发件人
+        msg.setFrom(new InternetAddress(username));
+        // 收件人
+        msg.setRecipients(Message.RecipientType.TO, addressesTo);
+        // 抄送
+        if (addressesCc != null) {
+            msg.setRecipients(Message.RecipientType.CC, addressesCc);
+        }
         msg.saveChanges();
 
         Transport transport = session.getTransport();
