e22f409a937a810f33f28bec260579790c451749
XanderYe <XanderYe@outlook.com>
2021-12-16 13:50:24 +0800

    升级依赖 重构DbUtil 删除js

--- src/main/java/cn/xanderye/util/DbUtil.java
+++ src/main/java/cn/xanderye/util/DbUtil.java
@@ -1,5 +1,8 @@
 package cn.xanderye.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
@@ -9,52 +12,89 @@ import java.util.*;
 /**
  * 数据库工具类
  */
+@Slf4j
 public class DbUtil {
 
-    private static Connection connection;
+    /**
+     * 连接地址
+     */
     private static String url;
+    /**
+     * 用户名
+     */
     private static String username;
+    /**
+     * 密码
+     */
     private static String password;
+    /**
+     * 驱动名称
+     */
     private static String driver;
-
+    /**
+     * 下划线
+     */
     private static final char UNDERLINE = '_';
 
-    // 类加载时先初始化连接
     static {
+        // 类加载时先初始化连接
         try {
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
             InputStream is = classLoader.getResourceAsStream("db.properties");
-            Properties p = new Properties();
-            p.load(is);
-            url = p.getProperty("db.url");
-            username = p.getProperty("db.username");
-            password = p.getProperty("db.password");
-            driver = p.getProperty("db.driver");
-
+            if (is != null) {
+                Properties p = new Properties();
+                p.load(is);
+                url = p.getProperty("db.url");
+                username = p.getProperty("db.username");
+                password = p.getProperty("db.password");
+                driver = p.getProperty("db.driver");
+            } else {
+                log.warn("Could not found db.properties, please configure manually.");
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Error initializing the config.");
         }
     }
 
     /**
-     * 初始化资源池
-     *
-     * @param
+     * 手动配置数据库配置
+     * @param customUrl
+     * @param customUsername
+     * @param customPassword
+     * @param customDriver
      * @return void
+     * @author XanderYe
+     * @date 2021/12/16
+     */
+    public static void setConfig(String customUrl, String customUsername, String customPassword, String customDriver) {
+        url = customUrl;
+        username = customUsername;
+        password = customPassword;
+        driver = customDriver;
+    }
+
+    /**
+     * 初始化连接池
+     * @param
+     * @return java.sql.Connection
+     * @author XanderYe
+     * @date 2021/12/16
      */
     public static Connection getConn() throws SQLException {
+        if (StringUtils.isAnyEmpty(url, username, password, driver)) {
+            throw new RuntimeException("Please check the database config.");
+        }
+        Connection conn = null;
         try {
-            if (connection == null || connection.isClosed()) {
-                Class.forName(driver);
-                connection = DriverManager.getConnection(url, username, password);
-            }
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        if (connection == null) {
-            throw new RuntimeException("Database connect failed.");
+        if (conn == null) {
+            throw new RuntimeException("Error connecting to the database.");
         }
-        return connection;
+        return conn;
     }
 
     /**
@@ -83,46 +123,16 @@ public class DbUtil {
         return affect;
     }
 
-    // 根据id查询
-    public static <T> T queryById(Class<T> clazz, String sql, int id) throws SQLException {
-        Connection conn = null;
-        T obj = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            conn = getConn();
-            ps = conn.prepareStatement(sql);
-            ps.setInt(1, id);
-            rs = ps.executeQuery();
-            if (rs.next()) {
-                Map<String, Object> map = getMapFromResultSet(rs);
-                obj = mapToObject(clazz, map);
-            }
-        } catch (IllegalArgumentException
-                | IllegalAccessException
-                | NoSuchFieldException
-                | SecurityException
-                | InstantiationException e) {
-            e.printStackTrace();
-        } finally {
-            close(rs, ps, conn);
-        }
-        return obj;
-    }
-
     /**
-     * 查询一条
-     *
-     * @param clazz
+     * 根据条件查询map
      * @param sql
      * @param args
-     * @return T
+     * @return java.util.Map<java.lang.String,java.lang.Object>
      * @author XanderYe
      * @date 2020/9/1
      */
-    public static <T> T queryOne(Class<T> clazz, String sql, Object... args) throws SQLException {
+    public static Map<String, Object> queryOne(String sql, Object... args) throws SQLException {
         Connection conn = null;
-        T obj = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
         try {
@@ -133,33 +143,47 @@ public class DbUtil {
             }
             rs = ps.executeQuery();
             if (rs.next()) {
-                Map<String, Object> map = getMapFromResultSet(rs);
-                obj = mapToObject(clazz, map);
+                return getMapFromResultSet(rs);
             }
-        } catch (IllegalArgumentException
-                | IllegalAccessException
-                | NoSuchFieldException
-                | SecurityException
-                | InstantiationException e) {
+        } catch (IllegalArgumentException | SecurityException e) {
             e.printStackTrace();
         } finally {
             close(rs, ps, conn);
         }
-        return obj;
+        return null;
     }
 
     /**
-     * 查询全部
-     *
+     * 根据条件查询对象
      * @param clazz
+     * @param sql
+     * @param args
+     * @return T
+     * @author XanderYe
+     * @date 2020/9/1
+     */
+    public static <T> T queryOne(Class<T> clazz, String sql, Object... args) throws SQLException {
+        Map<String, Object> map = queryOne(sql, args);
+        if (map != null) {
+            try {
+                return mapToObject(clazz, map);
+            } catch (IllegalAccessException | InstantiationException | NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 查询列表
      * @param sql
      * @param objs
      * @return java.util.List<T>
      * @author XanderYe
      * @date 2020/9/1
      */
-    public static <T> List<T> queryAll(Class<T> clazz, String sql, Object... objs) throws SQLException {
-        List<T> list = new ArrayList<>();
+    public static List<Map<String, Object>> queryList(String sql, Object... objs) throws SQLException {
+        List<Map<String, Object>> list = new ArrayList<>();
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -173,19 +197,39 @@ public class DbUtil {
 
             while (rs.next()) {
                 Map<String, Object> map = getMapFromResultSet(rs);
+                if (map != null) {
+                    list.add(map);
+                }
+            }
+        } catch (IllegalArgumentException | SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            close(rs, ps, conn);
+        }
+        return list;
+    }
+
+    /**
+     * 查询列表
+     * @param clazz
+     * @param sql
+     * @param objs
+     * @return java.util.List<T>
+     * @author XanderYe
+     * @date 2020/9/1
+     */
+    public static <T> List<T> queryList(Class<T> clazz, String sql, Object... objs) throws SQLException {
+        List<T> list = new ArrayList<>();
+        List<Map<String, Object>> mapList = queryList(sql, objs);
+        for (Map<String, Object> map : mapList) {
+            try {
                 T obj = mapToObject(clazz, map);
                 if (obj != null) {
                     list.add(obj);
                 }
+            } catch (NoSuchFieldException | IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
             }
-        } catch (IllegalArgumentException
-                | IllegalAccessException
-                | NoSuchFieldException
-                | SecurityException
-                | InstantiationException e) {
-            e.printStackTrace();
-        } finally {
-            close(rs, ps, conn);
         }
         return list;
     }
@@ -199,14 +243,17 @@ public class DbUtil {
      * @date 2021/2/1
      */
     private static Map<String, Object> getMapFromResultSet(ResultSet resultSet) throws SQLException {
-        Map<String, Object> map = new HashMap<>(16);
         ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
         int columnCount = resultSetMetaData.getColumnCount();
-        for (int i = 0; i < columnCount; i++) {
-            String columnName = resultSetMetaData.getColumnName(i + 1);
-            map.put(columnName, resultSet.getObject(columnName));
+        if (columnCount > 0) {
+            Map<String, Object> map = new HashMap<>(16);
+            for (int i = 0; i < columnCount; i++) {
+                String columnName = resultSetMetaData.getColumnName(i + 1);
+                map.put(columnName, resultSet.getObject(columnName));
+            }
+            return map;
         }
-        return map;
+        return null;
     }
 
     /**
@@ -226,9 +273,11 @@ public class DbUtil {
                 Object value = map.get(column);
                 String camel = underlineToCamel(column);
                 if (camel != null) {
-                    Field f = clazz.getDeclaredField(camel);
-                    f.setAccessible(true);
-                    f.set(object, value);
+                    try {
+                        Field f = clazz.getDeclaredField(camel);
+                        f.setAccessible(true);
+                        f.set(object, value);
+                    } catch (NoSuchFieldException ignored){}
                 }
             }
         }
